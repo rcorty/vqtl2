86fed305d539fdaf1effb5677da29fcb48e191a5
Robert Corty <rcorty@gmail.com>
2019-03-14 16:21:08 -0400

    make skeleton of effects_plot

---
+++ R/plots.R
@@ -0,0 +1,13 @@
+#' @title Plotting
+#'
+#' @param s1v the scan1var object
+#' @param focal_effects the effects to plot
+#'
+#' @return
+#' @export
+#'
+effects_plot <- function(s1v,
+                         focal_effects)
+{
+
+}
